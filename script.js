785b7938c24191769ede459c38a53d342792a71b
GeorginaBeese <georgina_beese@outlook.com>
2025-02-04 10:42:32 +0000

    Adding the human choice function

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-console.log("Hello World")
-
 
 function getComputerChoice() {
     const minCeiled = Math.ceil(1);
     const maxFloored = Math.floor(3);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    number = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    return number
   }
 
   console.log(getComputerChoice())
+
+
+function getHumanChoice () {
+  let choice = prompt("Select Rock, Paper or Scissors");
+  return choice.toLowerCase() === "rock" ? 1
+  : choice.toLowerCase() === "paper" ? 2
+  : choice.toLowerCase() === "scissors" ? 3
+  : undefined
+}
+
+console.log(getHumanChoice ())
