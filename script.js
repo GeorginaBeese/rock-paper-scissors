c35b5ea8e7a54d95cb9f73a07a28236e244e3994
GeorginaBeese <georgina_beese@outlook.com>
2025-02-05 10:25:08 +0000

    Creating the first round function

--- script.js
+++ script.js
@@ -6,15 +6,45 @@ function getComputerChoice() {
     return number
   }
 
-  console.log(getComputerChoice())
+//console.log(getComputerChoice())
 
 
-function getHumanChoice () {
+function getHumanChoice() {
   let choice = prompt("Select Rock, Paper or Scissors");
   return choice.toLowerCase() === "rock" ? 1
   : choice.toLowerCase() === "paper" ? 2
   : choice.toLowerCase() === "scissors" ? 3
-  : undefined
+  : undefined;
 }
 
-console.log(getHumanChoice ())
+
+
+function playRound(humanChoice, computerChoice) {
+  console.log(humanChoice)
+  console.log(computerChoice)
+
+  function readableChoice(choice) {
+    return choice === 1 ? "Rock"
+    : choice === 2 ? "Paper"
+    : choice === 3 ? "Scissors"
+    : undefined;
+  }
+
+  var humanScore = 0;
+  var computerScore = 0;
+
+  if (humanChoice === computerChoice) {
+    alert(`Its a draw! You both selected ${readableChoice(humanChoice)}!`);
+  }
+  else if (humanChoice === 1 && computerChoice === 3 || humanChoice === 2 && computerChoice === 1 || humanChoice === 3 && computerChoice === 2) {
+  humanScore = ++humanScore;
+    alert(`You WIN! ${readableChoice(humanChoice)} beats ${readableChoice(computerChoice)}!`);
+  }
+  else {
+    computerScore = ++computerScore;
+    alert(`You LOOSE! ${readableChoice(computerChoice)} beats ${readableChoice(humanChoice)}!`);
+  }
+  return humanScore
+}
+
+console.log(playRound( getHumanChoice(), getComputerChoice()))
